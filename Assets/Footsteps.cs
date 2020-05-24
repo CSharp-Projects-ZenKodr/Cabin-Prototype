52fc3324037e8519249a398da0027c1e95b05abe
Oliver Rankin-Starcevic <oliverrs1999@gmail.com>
2020-05-24 21:50:22 +1000

    Cabin, Creek, Monsters

---
+++ Assets/Footsteps.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public PhysicMaterial grassPhysMat;
+    public List<AudioClip> grassClipSet; 
+    public AudioSource mySource;
+
+
+    // Update is called once per frame
+    void OnCollisionStay(Collision collInfo)
+    {
+        if (collInfo.collider.material == grassPhysMat)
+        {
+            int toPlay = Random.Range(0, 10);
+            mySource.PlayOneShot(grassClipSet[toPlay], 0.9F);
+            mySource.Play();
+        }
+    }
+}
